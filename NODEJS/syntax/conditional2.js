859db7a16e3793495d1c44a5937db9288348bbd0
sanggeun <suki5038@kw.ac.kr>
2020-08-02 03:52:33 +0900

    콘솔환경에서 앱을 실행할 때 입력값을 전달하는 방법2

---
+++ NODEJS/syntax/conditional2.js
@@ -0,0 +1,14 @@
+var args = process.argv;
+console.log(args[2]); // 2번째 자리에 있는 거 출력.
+//0번째는 node //1번째는 주소 2번째는 입력한 값
+console.log('A');
+console.log('B');
+if(args[2] === '1')
+{
+  console.log('c1');
+}
+else if(true)
+{
+  console.log('c2');
+}
+console.log('d');
---
+++ NODEJS/syntax/number.js
@@ -0,0 +1,5 @@
+console.log(1+1);
+console.log(4-1);
+console.log(2*1);
+console.log(5/2);
+console.log(5%2);
