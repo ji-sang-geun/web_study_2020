0c4a3f1ebd8c3bf75452cead28f7d2b17e026251
sanggeun <suki5038@kw.ac.kr>
2020-08-02 04:08:41 +0900

    변수2

---
+++ NODEJS/syntax/variable2.js
@@ -0,0 +1,4 @@
+var name = 'Sanggeun'; // 중복을 제거하면 좋은 일이 생긴다.
+var letter = 'Dear '+ name + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt Sanggeun ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat '+ name + ' nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' + name + ' mollit anim id est laborum. Sangguen';
+console.log(letter); // 문자열 그대로
+console.log(letter.length); // 문자열 길이
