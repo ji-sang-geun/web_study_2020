3f07f46d664bad7336f6c375e53092cec04b5f07
sanggeun <suki5038@kw.ac.kr>
2020-08-02 02:34:56 +0900

    삭제 버튼 만들기

---
+++ NODEJS/syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6));// 2 ->
+console.log(Math.round(1.4));// 1
+
+function sum(first, second){ // 함수는 융통성이 떨어짐
+  // parameter : argument(인자)들을 받아서 함수로 넣어주는 매개체.
+  return first + second;
+}
+
+console.log(sum(2,4)); // argument : 입력값
---
+++ NODEJS/syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['jungjunha', 'aboda', 'jun jjang'];
+console.log(members[1]); //aboda
+
+var roles = {
+  'programmer' : 'junggjunyoung',
+  'designer' : 'jun jjang',
+  'manager' : 'jun young'
+}
+console.log(roles.designer); // jun jjang
