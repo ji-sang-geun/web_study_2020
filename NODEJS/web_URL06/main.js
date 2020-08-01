fb8432982a9c6b7d9a75c0a4796498b992560a0b
sanggeun <suki5038@kw.ac.kr>
2020-08-02 02:19:20 +0900

    홈페이지 구현

---
+++ NODEJS/web_URL06/main.js
@@ -0,0 +1,81 @@
+// sample URL http: // www.opentutorials.org:3000/main?id=HTML&page=12
+// http : 프로토콜 (어떤 방식으로 사용자가 통신할지)
+// opentutorials : host(domain) = 인터넷에 연결된 컴퓨터를 가르키는 주소
+// 3000 : port (3000번 포트에 연결된 서버와 통신)
+// main : path (어떤 디렉토리에 어떤 파일인지 가르킴)
+// query string (query string의 값에 따라서 달라짐)
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    console.log(url.parse(_url, true)); //path는 querystring 포함, pathname은 querystring이 포함되어 있어도 querystring을 제외한 path만 보여줌
+
+    if(pathname === '/')
+    {
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){ //필요없는 부분 왜냐하면 파일입출력을 사용하지 않음 main창은 불러오기 x
+          var title = 'Welcome to visit Homepage';
+          var description = "Hello, Node.js";
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      }); // 따라서 여기 부분도 필요가 없음.
+      }
+      else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+        });
+      }
+      }
+    else {
+      response.writeHead(404);
+      response.end('NOT FOUND');
+    }
+
+
+  });
+app.listen(3000);
