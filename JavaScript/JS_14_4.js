3654b92747616962ec9fe2de5b1c577308a05fd3
sanggeun <suki5038@naver.com>
2020-08-26 13:29:34 +0900

    유효범위의 대상

---
+++ JavaScript/JS_14_4.js
@@ -0,0 +1,8 @@
+for(var i = 0; i < 1; i++){
+    var name = 'Coding Everybody';
+}
+
+document.write(name); // Coding Everybody 출력
+
+// JS는 타 언어와 다르게 함수에 대한 유효범위만 제공
+// 이 말은 JS의 지역변수는 함수에서만 유효. (타 언어는 블록에 대한 유효범위 제공)
