c6e25912cf43b749c1093a6be73d1d53f67fbe69
sanggeun <suki5038@naver.com>
2020-09-01 01:21:53 +0900

    전역객체

--- JavaScript/JS_20.js
+++ JavaScript/JS_20.js
@@ -2,14 +2,14 @@ function func(){
     document.write('Hello? <br>');
 }
 
-func(); // Hello? 출력 // 함수
+func(); // Hello? 출력 //   
 window.func(); // Hello? 출력 // window = 객체, func() = 속성 괄호가 있으므로 함수, 따라서 메소드임.
 
 var o = {'func' : function(){
     document.write('Hello? <br>');
 }}
 o.func(); // Hello? 출력
-window.o.func(); // Hello? 출력
+globalThis.o.func(); // Hello? 출력
 
 // JS에서 모든 객체는 전역객체의 프로퍼티 
 // 전역 객체의 이름은 호스트 환경에 따라 이름이 달라질 수 있음
